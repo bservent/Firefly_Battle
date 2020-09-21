692182a4bc67ee3c6dc3339bdc8ce47b9d3d992b
Brittany Servent <brittybear@Brittanys-MBP.domain>
2020-09-21 08:25:35 -0700

    Dryer Code

--- scripts/app.js
+++ scripts/app.js
@@ -32,19 +32,20 @@ class Aliens extends Spaceship {
         this.accuracy = (Math.random()*(.8 - .6) + .6)
     }
     greet() {
-        console.log('Greetings Earthling!!! Prepare to DIE!')
+        console.log('%c Greetings Earthling!!! Prepare to DIE!', 'color: red')
     }
-    attack1(military1) {
-        military1.hull -= alien1.firepower
+    attack1(enemy) {
+        enemy.hull -= this.firepower
     }
 };
 
-const alien1 = new Aliens ('UNO'); 
-const alien2 = new Aliens ('DOS');
-const alien3 = new Aliens ('TRES');
-const alien4 = new Aliens ('QUATRO');
-const alien5 = new Aliens ('CINCO');
-const alien6 = new Aliens ('SEIS');
+const names = ['UNO', 'DOS', 'TRES', 'QUATRO', 'CINCO', 'SEIS'];
+const ships = [];
+for (let i = 0; i < 6; i++) {
+    const alien = new Aliens (names[i]);
+    ships.push(alien);
+}
+
 
 
 //__________________USS ASSEMBLY______________________________________
@@ -53,13 +54,13 @@ class Military extends Spaceship {
     constructor(name, hull, firepower, accuracy) {
         super (name, hull, firepower, accuracy)
     }
-    attack1(alien1) {
-        alien1.hull -= military1.firepower
+    attack1(enemy) {
+        enemy.hull -= military1.firepower
     }
     greet() {
-        console.log('You don\'t know who you\'r messing with!!!!')
+        console.log('%c You don\'t know who you\'r messing with!!!!', 'color: green')
     }
-
+    
 };
 
 const military1 = new Military ('USS Assembly', 20, 5, .7)
@@ -67,63 +68,38 @@ const military1 = new Military ('USS Assembly', 20, 5, .7)
 
 //_____________________ATTACK ATTACK ATTACK______________________________
 
-alien1.greet()
-military1.greet()
+function startGame() {
+    ships[0].greet()
+    military1.greet()
 
-while (military1.hull > 0 && alien1.hull > 0) {
-    military1.attack1(alien1);
-    console.log(`${military1.name} ATTACKS ${alien1.name}`);
-    alien1.attack1(military1);
-    console.log(`${alien1.name} ATTACKS ${military1.name}`);
+    while(ships.length > 0){
+            military1.attack1(ships[0]);
+            console.log(`%c ${military1.name} ATTACKS ${ships[0].name}`, 'color: green');
+            ships[0].attack1(military1);
+            console.log(`%c ${ships[0].name} ATTACKS ${military1.name}`, 'color: red');
+    
+
+        if (military1.hull <= 0 && ships[0].hull > 0) {
+            console.log(`%c ${military1.name} HAS BEEN DEFEATED!`, '36px; color: red');
+            console.log(`${ships[0].name} HAS WON!`, 'color: green;');
+        } else if (ships[0].hull <= 0 && military1.hull > 0) {
+            console.log(`%c ${ships[0].name} HAS BEEN DEFEATED!`, '36px; color: red');
+            console.log(`%c ${military1.name} HAS WON!`, 'color: green;');
+            ships.shift();
+            const promptSaying = prompt('Do you want to retreat? Yes or No.');
+            if (promptSaying === 'Yes') {
+                console.log('retreating')
+                console.log(`%c ${ships[0].name} HAS WON!!!`, '36px; color: red');
+                break 
+            }
+        }
+
+    }
+    if (military1.hull > 0) {
+        console.log('%c YOU HAVE WON!!!!!', 'color: green;')
+    }
 }
 
-alien2.greet()
-military1.greet()
 
-while (military1.hull > 0 && alien2.hull > 0) {
-    military1.attack1(alien2);
-    console.log(`${military1.name} ATTACKS ${alien2.name}`);
-    alien1.attack1(military1);
-    console.log(`${alien2.name} ATTACKS ${military1.name}`);
-}
 
-alien3.greet()
-military1.greet()
-
-while (military1.hull > 0 && alien3.hull > 0) {
-    military1.attack1(alien3);
-    console.log(`${military1.name} ATTACKS ${alien3.name}`);
-    alien3.attack1(military1);
-    console.log(`${alien3.name} ATTACKS ${military1.name}`);
-}
-
-alien4.greet()
-military1.greet()
-
-while (military1.hull > 0 && alien4.hull > 0) {
-    military1.attack1(alien4);
-    console.log(`${military1.name} ATTACKS ${alien4.name}`);
-    alien4.attack1(military1);
-    console.log(`${alien4.name} ATTACKS ${military1.name}`);
-}
-
-alien5.greet()
-military1.greet()
-
-while (military1.hull > 0 && alien5.hull > 0) {
-    military1.attack1(alien5);
-    console.log(`${military1.name} ATTACKS ${alien5.name}`);
-    alien5.attack1(military1);
-    console.log(`${alien5.name} ATTACKS ${military1.name}`);
-}
-
-alien6.greet()
-military1.greet()
-
-while (military1.hull > 0 && alien6.hull > 0) {
-    military1.attack1(alien6);
-    console.log(`${military1.name} ATTACKS ${alien6.name}`);
-    alien6.attack1(military1);
-    console.log(`${alien6.name} ATTACKS ${military1.name}`);
-}
-
+ 
